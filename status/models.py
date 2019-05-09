b2fdbe86a7c6eac0494b48d2059ad1b76983c3cf
dinstamaly <dinstamaly@gmail.com>
2019-05-09 17:57:34 +0600

    rename field 'updated'

--- status/models.py
+++ status/models.py
@@ -19,7 +19,7 @@ class Status(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to=upload_status_image, null=True, blank=True)
-    updates = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     objects = StatusManager
